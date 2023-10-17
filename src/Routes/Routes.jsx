b4ec56200591ac62e763932c4ae25449fc63c38c
tonoy3125 <shaifshajedt@gmail.com>
2023-10-17 21:22:15 +0600

    errorpage added commit

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,12 +2,21 @@ import {
     createBrowserRouter,
 } from "react-router-dom";
 import Root from "../Layout/Root";
+import ErrorElement from "../Layout/ErrorElement";
+import Home from "../Pages/Home/Home";
 
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Root></Root>,
+        errorElement: <ErrorElement></ErrorElement>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>
+            }
+        ]
     },
 ]);
 
