8ac42d47242821da0d0297bbd8a61a6fa49c2410
tonoy3125 <shaifshajedt@gmail.com>
2023-10-18 02:50:55 +0600

    slider section readded commit

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -28,44 +28,60 @@ const Banner = () => {
                 modules={[Autoplay, Navigation]}
                 className="mySwiper"
             >
-                <SwiperSlide className='relative'>
-                    <img src="https://i.ibb.co/xf1VFCF/woman-enjoying-her-financially-independence-while-buying-car.jpg" alt="" />
-                    <div data-aos="fade-down"
-                        data-aos-easing="linear"
-                        data-aos-duration="3000" className="absolute top-7 md:top-28 lg:top-48 md:left-5 lg:left-[650px] ">
-                        <h1 className="text-4xl md:text-6xl font-bold text-center mb-5">Find Your <span className='text-[#ff4605]'>Dream</span> Car</h1>
-                        <p className='md:text-base lg:text-base font-semibold mb-5 lg:mb-10 text-center text-[#842929]'>We can help you find the best car. Check our reviews, compare models and find cars for sale.</p>
-                        <button className='bg-[#ff635c] px-3 py-2 md:px-5 md:py-3 rounded-lg text-white text-center ml-36 md:ml-80 lg:ml-80'>Book Now</button>
+                <SwiperSlide>
+                    <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://i.ibb.co/3pPQzTx/dhiva-krishna-X16z-Xcbx-U4-U-unsplash.jpg)' }}>
+                        <div className="hero-overlay bg-opacity-60"></div>
+                        <div data-aos="fade-down"
+                            data-aos-easing="linear"
+                            data-aos-duration="3000" className="hero-content text-center text-neutral-content">
+                            <div className="">
+                                <h1 className="text-4xl md:text-6xl font-bold text-center mb-5">Find Your <span className='text-[#ff4605]'>Dream</span> Car</h1>
+                                <p className='md:text-base lg:text-base font-semibold mb-5 lg:mb-10 text-center text-[#842929]'>We can help you find the best car. Check our reviews, compare models and find cars for sale.</p>
+                                <button className='bg-[#ff635c] px-3 py-2 md:px-5 md:py-3 rounded-lg text-white text-center'>Book Now</button>
+                            </div>
+                        </div>
                     </div>
                 </SwiperSlide>
-                <SwiperSlide className='relative'>
-                    <img src="https://i.ibb.co/bKMwP2H/man-woman-closing-deal.jpg" alt="" />
-                    <div data-aos="fade-down"
-                        data-aos-easing="linear"
-                        data-aos-duration="3000" className="absolute top-7 md:top-28 lg:top-36 md:left-5 lg:left-[650px] ">
-                        <h1 className="text-4xl md:text-6xl font-bold text-center mb-5">Find Your <span className='text-[#ff4605]'>Dream</span> Car</h1>
-                        <p className='md:text-base lg:text-base font-semibold mb-5 lg:mb-10 text-center text-[#842929]'>We can help you find the best car. Check our reviews, compare models and find cars for sale.</p>
-                        <button className='bg-[#ff635c] px-3 py-2 md:px-5 md:py-3 rounded-lg text-white text-center ml-36 md:ml-80 lg:ml-80'>Book Now</button>
+                <SwiperSlide>
+                    <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://i.ibb.co/Qmr48nV/sebastiaan-stam-cc-E0s-QMMl5-M-unsplash.jpg)' }}>
+                        <div className="hero-overlay bg-opacity-60"></div>
+                        <div data-aos="fade-down"
+                            data-aos-easing="linear"
+                            data-aos-duration="3000" className="hero-content text-center text-neutral-content">
+                            <div className="">
+                                <h1 className="text-4xl md:text-6xl font-bold text-center mb-5">Find Your <span className='text-[#ff4605]'>Dream</span> Car</h1>
+                                <p className='md:text-base lg:text-base font-semibold mb-5 lg:mb-10 text-center text-[#842929]'>We can help you find the best car. Check our reviews, compare models and find cars for sale.</p>
+                                <button className='bg-[#ff635c] px-3 py-2 md:px-5 md:py-3 rounded-lg text-white text-center'>Book Now</button>
+                            </div>
+                        </div>
                     </div>
                 </SwiperSlide>
-                <SwiperSlide className='relative'>
-                    <img src="https://i.ibb.co/HLyYS6c/young-woman-choosing-car-car-showroom.jpg" alt="" />
-                    <div data-aos="fade-down"
-                        data-aos-easing="linear"
-                        data-aos-duration="3000" className="absolute top-7 md:top-28 lg:top-36 md:left-5 lg:left-[650px] ">
-                        <h1 className="text-4xl md:text-6xl font-bold text-center mb-5">Find Your <span className='text-[#ff4605]'>Dream</span> Car</h1>
-                        <p className='md:text-base lg:text-base font-semibold mb-5 lg:mb-10 text-center text-[#842929]'>We can help you find the best car. Check our reviews, compare models and find cars for sale.</p>
-                        <button className='bg-[#ff635c] px-3 py-2 md:px-5 md:py-3 rounded-lg text-white text-center ml-36 md:ml-80 lg:ml-80'>Book Now</button>
+                <SwiperSlide>
+                    <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://i.ibb.co/CHnP9rL/nathan-van-egmond-3ls0od-A7mfg-unsplash.jpg)' }}>
+                        <div className="hero-overlay bg-opacity-60"></div>
+                        <div data-aos="fade-down"
+                            data-aos-easing="linear"
+                            data-aos-duration="3000" className="hero-content text-center text-neutral-content">
+                            <div className="">
+                                <h1 className="text-4xl md:text-6xl font-bold text-center mb-5">Find Your <span className='text-[#ff4605]'>Dream</span> Car</h1>
+                                <p className='md:text-base lg:text-base font-semibold mb-5 lg:mb-10 text-center text-[#842929]'>We can help you find the best car. Check our reviews, compare models and find cars for sale.</p>
+                                <button className='bg-[#ff635c] px-3 py-2 md:px-5 md:py-3 rounded-lg text-white text-center'>Book Now</button>
+                            </div>
+                        </div>
                     </div>
                 </SwiperSlide>
-                <SwiperSlide className='relative'>
-                    <img src="https://i.ibb.co/CKr1ych/close-up-customer-with-business-person-car-dealership.jpg" alt="" />
-                    <div data-aos="fade-down"
-                        data-aos-easing="linear"
-                        data-aos-duration="3000" className="absolute top-7 md:top-28 lg:top-36 md:left-5 lg:left-[650px] ">
-                        <h1 className="text-4xl md:text-6xl font-bold text-center mb-5">Find Your <span className='text-[#ff4605]'>Dream</span> Car</h1>
-                        <p className='md:text-base lg:text-base font-semibold mb-5 lg:mb-10 text-center text-[#842929]'>We can help you find the best car. Check our reviews, compare models and find cars for sale.</p>
-                        <button className='bg-[#ff635c] px-3 py-2 md:px-5 md:py-3 rounded-lg text-white text-center ml-36 md:ml-80 lg:ml-80'>Book Now</button>
+                <SwiperSlide>
+                    <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://i.ibb.co/FBvDM7r/talia-PKd-QIG4go-Qc-unsplash.jpg)' }}>
+                        <div className="hero-overlay bg-opacity-60"></div>
+                        <div data-aos="fade-down"
+                            data-aos-easing="linear"
+                            data-aos-duration="3000" className="hero-content text-center text-neutral-content">
+                            <div className="">
+                                <h1 className="text-4xl md:text-6xl font-bold text-center mb-5">Find Your <span className='text-[#ff4605]'>Dream</span> Car</h1>
+                                <p className='md:text-base lg:text-base font-semibold mb-5 lg:mb-10 text-center text-[#842929]'>We can help you find the best car. Check our reviews, compare models and find cars for sale.</p>
+                                <button className='bg-[#ff635c] px-3 py-2 md:px-5 md:py-3 rounded-lg text-white text-center'>Book Now</button>
+                            </div>
+                        </div>
                     </div>
                 </SwiperSlide>
 
