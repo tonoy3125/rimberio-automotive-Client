7ab77cd339d7b36edd80113aa382544f064ddf62
tonoy3125 <shaifshajedt@gmail.com>
2023-10-18 17:28:11 +0600

    register pages authentication added commit

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,15 +1,74 @@
-import { useState } from "react";
 import { AiOutlineEye } from 'react-icons/ai';
 import { AiOutlineEyeInvisible } from 'react-icons/ai';
+import { useContext, useState } from "react";
+
+import { useNavigate } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
+import { AuthContext } from '../../Providers/Authprovider';
+
+
+
+
 
 const Register = () => {
     const [showPassword, setShowPassword] = useState(false)
+
+    const { createUser, logOut, handleUpdateProfile, setLoading } = useContext(AuthContext);
+
+    const navigate = useNavigate()
+
+    const handleRegister = e => {
+        e.preventDefault()
+        const name = e.target.name.value;
+        const photo = e.target.photo.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        const accepted = e.target.terms.checked;
+        console.log(name, email, password, photo, accepted)
+
+
+
+
+        if (!/^(?=.*[A-Z]).+$/
+            .test(password)) {
+            toast.error("Password must be One capital characters");
+            return
+        }
+        if (!/^(?=.*[\W_]).+$/.test(password)) {
+            toast.error("Password must be One special characters");
+            return
+        }
+        if (!/^.{6,}$/.test(password)) {
+            toast.error("Password must be at least 6 characters");
+            return
+        }
+
+
+
+        createUser(email, password)
+            .then(result => {
+                handleUpdateProfile(name, photo)
+                    .then(() => {
+
+                        logOut()
+
+                        navigate("/login")
+                    })
+                toast.success('User Create Successfully')
+
+
+            })
+            .catch(error => {
+                toast.error('Email Already In use try another one')
+                setLoading(false)
+            })
+    }
     return (
         <div className="bg-[#234756] pb-[180px] pt-9">
             <div className=" w-[400px] md:w-[752px]  mx-auto rounded-md bg-[#fff] pt-10 md:pt-[76px]">
                 <h1 className=" text-2xl md:text-4xl font-semibold text-center text-[#234756] ">Register Your Account</h1>
                 <hr className="md:w-[606px] h-1 mt-7 md:mt-12 mb-7 bg-black md:mb-12 mx-auto" />
-                <form className="pl-1 md:pl-24">
+                <form onSubmit={handleRegister} className="pl-1 md:pl-24">
                     <div className="mb-6">
                         <h2 className=" text-base md:text-xl font-semibold text-black mb-4">Your Name</h2>
                         <input className="pt-5 pb-5 pl-2 md:p-5 w-[390px] md:w-[558px] border border-black text-base font-normal text-[#9F9F9F] rounded" type="text" name="name" placeholder="Enter your name" />
@@ -42,7 +101,7 @@ const Register = () => {
                     </div>
                 </form>
             </div>
-            
+            <Toaster />
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './Routes/Routes.jsx'
+import Authprovider from './Providers/Authprovider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Authprovider><RouterProvider router={router} /></Authprovider>
   </React.StrictMode>,
 )
